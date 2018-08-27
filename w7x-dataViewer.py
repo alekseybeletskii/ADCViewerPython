2819bf99aefe7514245319b512b8c4ad55239170
Aleksey Beletskii <beletskiial@gmail.com>
2018-08-28 01:28:02 +0300

    savitzky-golay filter implemented

--- w7x-dataViewer.py
+++ w7x-dataViewer.py
@@ -17,6 +17,7 @@ import pandas as pd
 
 import MDSplus as m
 
+from scipy.signal import savgol_filter
 
 class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
 
@@ -28,10 +29,10 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
         self.t = []
         self.channelsList = []
         self.nextPen = 0
-
         self.setupUi(self)  # This is defined in design.py file automatically
         # It sets up layout and widgets that are defined
 
+        self.fs = self.samplingRate_kHz.text()
         self.actionOpen_csv.triggered.connect(self.openCsv)
         self.actionDrawPlotsFromCsv.triggered.connect(self.drawPlotsFromCsv)
         self.actionOpen_mdsplus_QXT.triggered.connect(self.openMdsplusQXT)
@@ -42,6 +43,12 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
 
         self.actionClear.triggered.connect(self.clearPlots)
         self.actionExit.triggered.connect(self.exitApp)
+        self.xLeft=0
+        self.xRight=0
+
+        self.redraw.clicked.connect(self.drawPlotsFromMdsplus)
+
+
 
     #        plotexample(self)
 
@@ -69,7 +76,7 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
         options |= QFileDialog.DontUseNativeDialog
         #        files, _ = QFileDialog.getOpenFileNames(None,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
         #         files, _ = QFileDialog.getOpenFileNames(None,"QFileDialog.getOpenFileNames()", "csv files (*.csv)","csv files (*.csv);;All Files (*)", options=options)
-        self.files, _ = QFileDialog.getOpenFileNames(None, "QFileDialog.getOpenFileNames()", "All Files (*)",
+        self.files, _ = QFileDialog.getOpenFileNames(self, None, "QFileDialog.getOpenFileNames()", "All Files (*)",
                                                      "All Files (*)", options=options)
 
     def drawPlotsFromCsv(self):
@@ -84,17 +91,12 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
         sys.exit()
 
     def openMdsplusQXT(self):
-        self.clearPlots()
+        # self.clearPlots()
         self.readChannelsList()
-
-        tQ = self.timeScale.text()
-        tQ = tQ if int(tQ) > 0 else '1000000000'
-        self.timeScale.setText(tQ)
-        tQ = "SETTIMECONTEXT(*,*," + tQ + "Q)"
 
         c = m.Connection('mds-data-1')
         # c = m.Connection('ssh://oleb@mds-trm-1.ipp-hgw.mpg.de')
-        c.get(tQ)
+        c.get(self.setTimeContext())
         # c.get('SETTIMECONTEXT(*,*,10000Q)')
         # c.openTree('qxt1', 180816020)
         # c.openTree('qxt1', 171123034)
@@ -105,56 +107,75 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
         c.openTree('qxt1', shotNumber)
 
         for i in range(len(self.channelsList)):
-            self.d.append(c.get(f'DATA:{self.channelsList[i]}'))
-            self.t.append(c.get(f'DIM_OF(DATA:{self.channelsList[i]})'))
+            # dat_raw = c.get('DATA:CH84')
+            dat_raw = c.get(f'DATA:{self.channelsList[i]}')
+            # t_raw = c.get(f'DIM_OF(DATA:{self.channelsList[i]})')
+            t_raw = np.double(dat_raw.dim_of().data())
+
+            self.d.append(dat_raw)
+            self.t.append(t_raw)
 
         print('data loaded from mdsplus')
+        # print('fs: ',self.fs)
+
+    def setTimeContext(self):
+        resample = int(self.resampling.text())
+        settimecontext = "SETTIMECONTEXT(*,*," + str(resample) + "Q)"
+        if resample < 0:
+            resample = '1000000'
+            settimecontext = "SETTIMECONTEXT(*,*," + str(resample) + "Q)"
+            self.timeScale.setText(str(resample))
+        if resample == 1:
+            settimecontext = "SETTIMECONTEXT(*,*,*)"
+        return settimecontext
 
     def openMdsplusQOC(self):
-        self.clearPlots()
         # mdpid = 171207017  # PCI saw activity here
         # mdpid = 180823005
+        #
+        # conn = MDSplus.Connection(MDSconnect)
+        # conn.openTree('w7x', mdpid)
+        # MDSraw = conn.get('\W7X::TOP.QOC.DATAET2CH16')
+        # dat_raw = MDSraw.data()
+        # fs = np.int(conn.get('\W7X::TOP.QOC.HARDWARE:ACQ2106_064:CLOCK'))
+        # t_raw = np.double(MDSraw.dim_of().data()) / fs
 
+        # self.clearPlots()
         self.readChannelsList()
-
-        tQ = self.timeScale.text()
-        tQ = tQ if int(tQ) > 0 else '1000000000'
-        self.timeScale.setText(tQ)
-        tQ = "SETTIMECONTEXT(*,*," + tQ + "Q)"
 
         shotNumber = self.shot.text()
         shotNumber = int(shotNumber) if len(shotNumber)==9 else 180823005
         self.shot.setText(str(shotNumber))
 
         c = m.Connection('mds-data-1')
-#        c = m.Connection('ssh://oleb@mds-trm-1.ipp-hgw.mpg.de')
+        #        c = m.Connection('ssh://oleb@mds-trm-1.ipp-hgw.mpg.de')
 
-        c.get(tQ)
+        c.get(self.setTimeContext())
+
 
         c.openTree('qoc', shotNumber)
-        fs = np.int(c.get('HARDWARE:ACQ2106_064:CLOCK'))
+        # fs = np.int(c.get('HARDWARE:ACQ2106_064:CLOCK'))
 
         for i in range(len(self.channelsList)):
             # MDSraw = c.get('DATA:DET2CH16')
-            # MDSraw = c.get(f'DATA:{self.channelsList[i]}')
-            # dat_raw = MDSraw.data()
-            # t_raw = np.double(MDSraw.dim_of().data()) / fs
-
             dat_raw = c.get(f'DATA:{self.channelsList[i]}')
-            t_raw = c.get(f'DIM_OF(DATA:{self.channelsList[i]})')
+            # dat_raw = MDSraw.data()
+            t_raw = np.double(dat_raw.dim_of())
+            # t_raw = np.double(dat_raw.dim_of())/fs
+
+            # dat_raw = c.get(f'DATA:{self.channelsList[i]}')
+            # t_raw = c.get(f'DIM_OF(DATA:{self.channelsList[i]})')
 
             self.d.append(dat_raw)
             self.t.append(t_raw)
 
         print('data loaded from mdsplus')
-        print('fs: ',fs)
+        # print('fs: ',fs)
 
     def export_to_csv_v1(self):
         # output to file
 
-        axX = self.plot.plotItem.getAxis('bottom')
-        xLeft = int(axX.range[0])
-        xRight = int(axX.range[1])
+
         # print(xLeft)
         # print(xRight)
 
@@ -169,19 +190,16 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
             signal = self.d[i]
             time = self.t[i]
 
-            xLeft = xLeft if xLeft > 0 else 0
-            xRight = xRight if xRight < len(signal) else len(signal) - 1
+            xLeft = self.xLeft if self.xLeft > 0 else 0
+            xRight = self.xRight if self.xRight < len(signal) else len(signal) - 1
             np.savetxt(filepath, np.array([time[xLeft:xRight], signal[xLeft:xRight]]).T, delimiter=', ')
-        print('xLeft: ',xLeft)
-        print('xRight: ',xRight)
+
         print('data exported to csv files')
 
     def export_to_csv_v2(self):
         # output to file
 
-        axX = self.plot.plotItem.getAxis('bottom')
-        xLeft = int(axX.range[0])
-        xRight = int(axX.range[1])
+
         # print(xLeft)
         # print(xRight)
 
@@ -195,8 +213,8 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
             signal = self.d[i]
             time = self.t[i]
 
-            xLeft = xLeft if xLeft > 0 else 0
-            xRight = xRight if xRight < len(signal) else len(signal) - 1
+            xLeft = self.xLeft if self.xLeft > 0 else 0
+            xRight = self.xRight if self.xRight < len(signal) else len(signal) - 1
 
             np.savetxt(filepath + "_time_" + ".csv", time[xLeft:xRight])
             np.savetxt(filepath + "_data_" + ".csv", signal[xLeft:xRight])
@@ -204,25 +222,31 @@ class mainApp(QtGui.QMainWindow, mainLayout.Ui_MainWindow):
             # df = pd.DataFrame(np.array([time[xLeft:xRight], signal[xLeft:xRight]]).T,index=None, columns=None)
             # df.to_csv(filename, header=None, index=None)
 
-        print('xLeft: ',xLeft)
-        print('xRight: ',xRight)
-        print('data exported to csv files')
+        print('data exported to csv files, time separated')
 
     def drawPlotsFromMdsplus(self):
+        self.plot.clear()
         for i in range(len(self.d)):
             signal = self.d[i]
-            time = list(range(len(signal)))
-            # time = self.t[i]
+            # time = list(range(len(signal)))
+            time = self.t[i]
             self.nextPen = self.nextPen + 1
             # self.plot.plot(time,signal, pen=(self.nextPen))
             # self.plot.plot(time[0:len(signal)],signal, pen=(self.nextPen))
-            self.plot.plot(time, signal, pen=(self.nextPen))
+            self.plot.plot(time, signal, pen=(self.nextPen),)
+            if self.SGFilt.checkState():
+               smoothed = self.savitzky_golay_filt(signal,int(self.winLength.text()),int(self.polyOrder.text()))
+               self.plot.plot(time, smoothed, pen=0)
 
         axX = self.plot.plotItem.getAxis('bottom')
+        self.xLeft = int(axX.range[0])
+        self.xRight = int(axX.range[1])
         print('x axis range: {}'.format(axX.range))  # <------- get range of x axis
         axY = self.plot.plotItem.getAxis('left')
         print('y axis range: {}'.format(axY.range))  # <------- get range of y axis
 
+    def savitzky_golay_filt(self,data, window_length=1001, polyorder=0, deriv=0, delta=1.0, axis=-1, mode='interp'):
+        return savgol_filter(data,window_length,polyorder,mode=mode)
 
 def main():
     app = QtGui.QApplication(sys.argv)
